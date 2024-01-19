a986c74248b904e85e9759c55080cf52a0fbc1e9
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2024-01-19 23:34:27 +0530

    Sticky Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/app/Navbar';
 import React from 'react';
 import Footer from './components/Home/Footer';
 import Head from 'next/head';
+import clsx from 'clsx';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,11 +14,7 @@ export const metadata: Metadata = {
   description: 'The Landing Page',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <Head>
@@ -26,8 +23,10 @@ export default function RootLayout({
           href='https://img.icons8.com/arcade/64/overview-pages-2.png'
         />
       </Head>
-      <body className={inter.className}>
-        <Navbar />
+      <body className={clsx(inter.className, 'relative')}>
+        <div className='sticky top-0'>
+          <Navbar />
+        </div>
         <main>{children}</main>
         <Footer />
       </body>
