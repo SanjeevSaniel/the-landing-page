5928050e7156725fa9fab41418db0b68451d357c
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2024-02-06 15:09:45 +0530

    Refactor: Footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -7,28 +7,23 @@ const Footer = () => {
   return (
     <section className='flex justify-between items-center px-20 py-5 font-medium'>
       <div>&copy; {new Date().getFullYear()}, SK. Connecting is better.</div>
-
-      
-
-
       <Tooltip
-          placement='top'
-          content='Next.js'
-          color="foreground"
-        >
-          <Link
-        href='https://nextjs.org/'
-        target='_blank'
-        className='flex items-center gap-1 text-tiny'>
-        <span className='hover:text-underline'>built on</span>
-        <Image
-          width={20}
-          height={20}
-          src='https://img.icons8.com/fluency/48/nextjs.png'
-          alt='Next.js'
-        />
-      </Link>
-        </Tooltip>
+        placement='top'
+        content='Next.js'
+        color='foreground'>
+        <Link
+          href='https://nextjs.org/'
+          target='_blank'
+          className='flex items-center gap-1 text-tiny'>
+          <span className='hover:text-underline'>built on</span>
+          <Image
+            width={20}
+            height={20}
+            src='https://img.icons8.com/fluency/48/nextjs.png'
+            alt='Next.js'
+          />
+        </Link>
+      </Tooltip>
     </section>
   );
 };
